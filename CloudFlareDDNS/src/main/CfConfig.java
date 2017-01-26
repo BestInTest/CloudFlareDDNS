f715897b4e1cbab0581e0d8b403d2885bbc7e200
k1461615 <austin.lautissier@kcl.ac.uk>
2017-01-26 16:09:59 +0000

    Created GUI for application.

--- CloudFlareDDNS/src/main/CfConfig.java
+++ CloudFlareDDNS/src/main/CfConfig.java
@@ -1,9 +1,6 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,17 +9,37 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public class CfConfig {
+class CfConfig {
 	private String email, key, zoneId, zoneName, wanIp;
 	private ArrayList<JSONObject> zoneDnsList;
 	private String webUrl = "https://api.cloudflare.com/client/v4/zones/";
-	private int maxRetries = 2;
 
-	public String getServerName() {
+	/**
+	 * @return The name of the server
+	 */
+	String getServerName() {
 		return zoneName;
 	}
 
-	public CfConfig(String email, String key) {
+    /**
+     * Returns stored WAN IP.
+     *
+     * @return Wan IP
+     */
+    String getWanIp() {
+        return wanIp;
+    }
+
+    /**
+     * Returns stored list of type A DNS records.
+     *
+     * @return List of type A DNS records
+     */
+    ArrayList<JSONObject> getDnsList() {
+        return zoneDnsList;
+    }
+
+    CfConfig(String email, String key) throws Exception {
 		this.email = email;
 		this.key = key;
 
@@ -30,14 +47,10 @@ public class CfConfig {
 		refreshDnsRecords();
 	}
 
-	public ArrayList<JSONObject> getDnsList() {
-		return zoneDnsList;
-	}
-
 	/**
 	 * Fetches and stores DNS records from CloudFlare.
 	 */
-	public void refreshDnsRecords() {
+	void refreshDnsRecords() throws Exception {
 		refreshWanIp();
 		HttpURLConnection connection = null;
 
@@ -57,7 +70,7 @@ public class CfConfig {
 			JSONObject jsonObject = (JSONObject) parser.parse(br.readLine());
 			JSONArray result = (JSONArray) jsonObject.get("result");
 			br.close();
-			zoneDnsList = new ArrayList<JSONObject>();
+			zoneDnsList = new ArrayList<>();
 
 			// Check if DNS record content matches WAN IP
 			for (int i = 0; i < result.size(); i++) {
@@ -70,15 +83,6 @@ public class CfConfig {
 					break;
 				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-
-			if (maxRetries > 0) {
-				refreshDnsRecords();
-				maxRetries--;
-			} else {
-				System.exit(1);
-			}
 		} finally {
 			if (connection != null) {
 				connection.disconnect();
@@ -89,7 +93,7 @@ public class CfConfig {
 	/**
 	 * Fetches and stores Zone details from CloudFlare.
 	 */
-	private void refreshZoneId() {
+	private void refreshZoneId() throws Exception {
 		HttpURLConnection connection = null;
 
 		try {
@@ -111,17 +115,8 @@ public class CfConfig {
 			JSONObject zoneInfo = (JSONObject) result.get(0);
 			zoneId = (String) zoneInfo.get("id");
 			zoneName = (String) zoneInfo.get("name");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-
-			if (maxRetries > 0) {
-				refreshZoneId();
-				maxRetries--;
-			} else {
-				System.exit(1);
-			}
-		} finally {
+		}
+		finally {
 			if (connection != null) {
 				connection.disconnect();
 			}
@@ -131,10 +126,8 @@ public class CfConfig {
 	/**
 	 * Updates content of the DNS records on CloudFlare. Returns boolean based
 	 * on success of API call.
-	 * 
-	 * @return success of API call
 	 */
-	private void updateZoneDns(JSONObject data) {
+	private void updateZoneDns(JSONObject data) throws Exception {
 		HttpURLConnection connection = null;
 
 		try {
@@ -159,16 +152,6 @@ public class CfConfig {
 			// Get server response
 			InputStream is = connection.getInputStream();
 			is.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-
-			if (maxRetries > 0) {
-				updateZoneDns(data);
-				maxRetries--;
-			} else {
-				System.exit(1);
-			}
 		} finally {
 			if (connection != null) {
 				connection.disconnect();
@@ -179,7 +162,7 @@ public class CfConfig {
 	/**
 	 * Fetches and stores local IP address using Amazon Web Services.
 	 */
-	private void refreshWanIp() {
+	private void refreshWanIp() throws Exception {
 		HttpURLConnection connection = null;
 
 		try {
@@ -192,15 +175,6 @@ public class CfConfig {
 			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 			wanIp = br.readLine();
 			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-
-			if (maxRetries > 0) {
-				refreshDnsRecords();
-				maxRetries--;
-			} else {
-				System.exit(1);
-			}
 		} finally {
 			if (connection != null) {
 				connection.disconnect();
---
+++ CloudFlareDDNS/src/main/Gui.java
@@ -0,0 +1,278 @@
+package main;
+
+import org.json.simple.JSONObject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+/**
+ * @author Austin Lautissier
+ *
+ */
+public class Gui {
+    private static JFrame frame = new JFrame("CloudFlareDDNS v1.0");
+    private static boolean autoRefresh = false;
+
+    public static void main(String[] args) {
+        frame.setLocation(200, 200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        loginGui();
+    }
+
+    private static void loginGui(){
+        frame.getContentPane().removeAll();
+        frame.setLayout(new GridBagLayout());
+
+        JLabel emailLabel = new JLabel("Email");
+        JTextField emailTf = new JTextField(30);
+        JLabel keyLabel = new JLabel("Key");
+        JTextField keyTf = new JTextField(30);
+        JButton loginBut = new JButton("Log In");
+        loginBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new CfLoginTask(emailTf.getText(), keyTf.getText()).execute();
+                loadingGui("Connecting to server...");
+            }
+        });
+
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.insets = new Insets(10, 10, 10, 10);
+
+        frame.add(emailLabel, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 0;
+        frame.add(emailTf, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 1;
+        frame.add(keyLabel ,gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        frame.add(keyTf ,gc);
+
+        gc.gridx = 2;
+        gc.gridy = 2;
+        frame.add(loginBut ,gc);
+
+        frame.pack();
+    }
+
+    private static void loadingGui(String message) {
+        frame.getContentPane().removeAll();
+
+        JPanel loadingPanel = new JPanel(new FlowLayout());
+        frame.add(loadingPanel);
+
+        JLabel loadingLabel = new JLabel(message);
+        loadingPanel.add(loadingLabel);
+
+        frame.getContentPane().revalidate();
+        frame.repaint();
+    }
+
+    private static void showMessageBox(String message){
+        JOptionPane.showMessageDialog(null, message);
+    }
+
+    private static void serverSelectGui(CfConfig config){
+        frame.getContentPane().removeAll();
+        frame.setLayout(new BorderLayout());
+        String ar;
+        if (autoRefresh) {
+            ar = "ON";
+        } else {
+            ar = "OFF";
+        }
+
+        JLabel snLabel = new JLabel("Server name: " + config.getServerName() + "     WAN IP: " + config.getWanIp() + "     Auto refresh: " + ar);
+        frame.add(snLabel, BorderLayout.PAGE_START);
+
+        ArrayList<JSONObject> dnsList = config.getDnsList();
+        String[] colNames = {"Name", "Type", "Content"};
+        Object[][] data = new Object[dnsList.size()][colNames.length];
+
+        for (int i = 0; i < dnsList.size(); i++) {
+            JSONObject dnsJson = dnsList.get(i);
+
+            data[i][0] = dnsJson.get("name");
+            data[i][1] = dnsJson.get("type");
+            data[i][2] = dnsJson.get("content");
+        }
+
+        JTable table = new JTable(data, colNames);
+        JScrollPane sp = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+        frame.add(sp, BorderLayout.CENTER);
+
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+
+        JButton refreshBut = new JButton("Refresh");
+        refreshBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                (new CfRefreshTask(config)).execute();
+                loadingGui("Refreshing DNS records...");
+            }
+        });
+        buttonPanel.add(refreshBut);
+
+        JButton autoRefreshBut = new JButton("Auto refresh");
+        autoRefreshBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                autoRefreshGui(config);
+            }
+        });
+        buttonPanel.add(autoRefreshBut);
+
+        frame.add(buttonPanel, BorderLayout.PAGE_END);
+
+        frame.pack();
+    }
+
+    private static void autoRefreshGui(CfConfig config){
+        frame.getContentPane().removeAll();
+        frame.setLayout(new GridBagLayout());
+
+        JLabel timeLabel = new JLabel("Minutes between updates: ");
+        JComboBox<Integer> numberCombo = new JComboBox<>(new Integer[] {5, 15, 30, 60, 720, 1440});
+        JLabel autoRefreshLabel = new JLabel("Toggle auto refresh on/off:");
+        JCheckBox arCheckBox = new JCheckBox();
+
+        JButton okBut = new JButton("OK");
+        okBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (arCheckBox.isSelected()) {
+                    int mins = (int) numberCombo.getSelectedItem();
+                    autoRefresh = true;
+                    new CfAutoRefreshTask(config, mins).execute();
+                } else{
+                    autoRefresh = false;
+                }
+                serverSelectGui(config);
+            }
+        });
+        JButton cancelBut = new JButton("Cancel");
+        cancelBut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                serverSelectGui(config);
+            }
+        });
+
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        gc.insets = new Insets(10, 10, 10, 10);
+
+        frame.add(timeLabel, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 0;
+        frame.add(numberCombo, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 1;
+        frame.add(autoRefreshLabel, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        frame.add(arCheckBox, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 2;
+        frame.add(okBut, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 2;
+        frame.add(cancelBut, gc);
+
+        frame.revalidate();
+        frame.repaint();
+    }
+
+    private static class CfRefreshTask extends SwingWorker<Void, Void>{
+        CfConfig config;
+
+        CfRefreshTask(CfConfig config){
+            this.config = config;
+        }
+
+        @Override
+        public Void doInBackground() {
+            try {
+                config.refreshDnsRecords();
+                showMessageBox("Refreshed records");
+            } catch (Exception e) {
+                showMessageBox("Failed to refresh records. Please check internet connectivity");
+            }
+            return null;
+        }
+
+        @Override
+        public void done(){
+            serverSelectGui(config);
+        }
+    }
+
+    private static class CfAutoRefreshTask extends SwingWorker<Void, Void>{
+        CfConfig config;
+        int mins;
+
+        CfAutoRefreshTask(CfConfig config, int mins){
+            this.config = config;
+            this.mins = mins;
+        }
+
+        @Override
+        protected Void doInBackground() throws Exception {
+            while (autoRefresh){
+                Thread.sleep(mins * 60000);
+                config.refreshDnsRecords();
+            }
+            return null;
+        }
+    }
+
+    private static class CfLoginTask extends SwingWorker<Void, Void>{
+        String email, key;
+        boolean success = false;
+        CfConfig config;
+
+        CfLoginTask(String email, String key){
+            this.email = email;
+            this.key = key;
+        }
+
+        @Override
+        public Void doInBackground() {
+            try {
+                config = new CfConfig(email, key);
+                success = true;
+            } catch (Exception e) {
+                showMessageBox("Invalid email or key");
+            }
+            return null;
+        }
+
+        @Override
+        public void done(){
+            if (success) {
+                serverSelectGui(config);
+            } else {
+                loginGui();
+            }
+        }
+
+    }
+}
